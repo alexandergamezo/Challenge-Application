c55ad664dd81cf4718b25237cc668344fec05e12
alex_ga <alex_ga@holdingbp.ru>
2024-02-11 21:11:41 +0300

    feat: implement a money withdrawal

---
+++ Application/Features/WithdrawMoney.cs
@@ -0,0 +1,38 @@
+﻿using Application.DataAccess;
+using Application.Domain.Services;
+using System;
+using Application.Domain;
+
+namespace Application.Features
+{
+    public class WithdrawMoney
+    {
+        private IAccountRepository accountRepository;
+        private INotificationService notificationService;
+
+        public WithdrawMoney(IAccountRepository accountRepository, INotificationService notificationService)
+        {
+            this.accountRepository = accountRepository;
+            this.notificationService = notificationService;
+        }
+
+        public void Execute(Guid fromAccountId, decimal amount)
+        {
+            var account = accountRepository.GetAccountById(fromAccountId);
+
+            var balance = account.Balance - amount;
+            if (balance < 0m)
+            {
+                throw new InvalidOperationException("Insufficient funds to make transfer");
+            }
+
+            if (balance < 500m)
+            {
+                notificationService.NotifyFundsLow(account.User.Email);
+            }
+
+            account.Balance -= amount;
+            accountRepository.Update(account);
+        }
+    }
+}
